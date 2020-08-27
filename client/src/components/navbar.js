02424ce9f5cd70242d836caa1d5780c12b20885d
walteralleyz <walterdasilvasantos@gmail.com>
2020-08-27 11:49:00 -0300

    components added, lack of ideas

---
+++ client/src/components/navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Batman from "../images/avatar.png";
+
+const Navbar = ({ logged }) => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li className="navbar__item">
+                    <h1>|MakeMeQuizz|</h1>
+                </li>
+                <li className="navbar__item">
+                    <h5>{logged ? "0 pontos" : "Quiz"}</h5>
+                </li>
+                <li className="navbar__item">
+                    <img
+                        className="navbar__profpic"
+                        src={Batman}
+                        alt="profile"
+                    />
+                    <small>{logged ? "Sair" : "Entrar"}</small>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ client/src/routes/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Navbar from '../components/navbar';
+
+function Signup() {
+
+    return (
+        <Navbar logged={false} />
+    )
+}
+
+export default Signup;
